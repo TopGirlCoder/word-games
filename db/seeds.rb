2ccf28df3dbdc2cc4d3c2dd512375481491cca4e
Tanya Lindermeier <topgirlcoder@gmail.com>
2017-08-23 22:42:42 -0700

    Create file to seed the database with Word objects read from the dictionary text file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Delete all words from previous seeds
+Word.delete_all
+
+# Instantiate words from a word file into Word objects to seed the database
+File.foreach('test/fixtures/words.txt') do |line|
+	Word.create(entry: line.chomp)
+end	
